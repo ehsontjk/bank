7e9b034ed8936aac484822b9d27284b18aabe6f4
Ekhsondzhon Boboev <ehsonboboev@gmail.com>
2020-09-19 02:37:46 +0500

    Package stats and test added

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,14 @@
+package types
+type Money int64
+
+type Category string
+
+	
+
+    
+
+type Payment struct {
+	ID int // 'card'
+	Amount Money // номер вида '5058 xxxx xxxx 8888'
+	Category Category // баланс в дирамах
+}
