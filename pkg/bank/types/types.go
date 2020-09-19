570f1a0acc9ddcb90570a756addb3a596adf81d2
Ekhsondzhon Boboev <ehsonboboev@gmail.com>
2020-09-19 20:17:45 +0500

    Types added

--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package types
 type Money int
 
@@ -12,19 +12,5 @@ type Payment struct {
 	ID int // 'card'
 	Amount Money // номер вида '5058 xxxx xxxx 8888'
 	Category Category // баланс в дирамах
-=======
-package types
-type Money int
 
-type Category string
-
-	
-
-    
-
-type Payment struct {
-	ID int // 'card'
-	Amount Money // номер вида '5058 xxxx xxxx 8888'
-	Category Category // баланс в дирамах
->>>>>>> c9c629393c3eb4f2af0e79d551799205339fc812
 }
